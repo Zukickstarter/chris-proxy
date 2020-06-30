6c42d829a2f8d8054e95254a5d98081cd04300ad
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-06-29 17:28:00 -0700

    files created: index.html symlink .gitignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+const pledgesComponentSource = require('./pledges_component');
+
+
+
 /*
-create simlink with ln -s on the command line to point to
-
-
+- create simlink with ln -s on the command line to point to pledges component src file
+- require simlink so that when webpack is pointed at this file, it fetches all the source code in the pledges directory
 */
+
