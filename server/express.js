38ecbd809971f821e7fe3858a03cccf1ab6d7862
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-06-30 11:16:34 -0700

    clean up

--- server/express.js
+++ server/express.js
@@ -6,7 +6,7 @@ app.use(express.static('dist'));
 app.use(express.json());
 const port = 3000;
 
-var apiProxy = httpProxy.createProxyServer({ prependPath: false });
+const apiProxy = httpProxy.createProxyServer({ prependPath: false });
 
 app.get("/api/pledges/:id", function(req, res) {
     let { id } = req.params;
@@ -14,8 +14,9 @@ app.get("/api/pledges/:id", function(req, res) {
     apiProxy.web(req, res, {target: `http://localhost:3003/api/pledges/${id}`});
 });
 
-apiProxy.on('error', function(e) {
-  console.log('proxy error: ', e);
+apiProxy.on('error', function(err) {
+  console.log('proxy error: ', err);
 });
 
 app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
+
