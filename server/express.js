800572140f0ea4df57741982de008025639d0c46
Christopher Tucker <ChrisTuckerAudio+github@gmail.com>
2020-07-01 17:33:23 -0700

    all three components rendering: - bug prevents data from photos_component and from description_component from being retrieved.

--- server/express.js
+++ server/express.js
@@ -10,16 +10,21 @@ const apiProxy = httpProxy.createProxyServer({ prependPath: false });
 
 app.get("/api/pledges/:id", function(req, res) {
   let { id } = req.params;
-  console.log('pledges enpoint hit with id: ', id);
+  console.log('pledges endpoint hit with id: ', id);
   apiProxy.web(req, res, {target: `http://localhost:3003/api/pledges/${id}`});
 });
 
 app.get('/api/description/:id', function(req, res) {
   let { id } = req.params;
-  console.log('description enpoint hit with id: ', id);
+  console.log('description endpoint hit with id: ', id);
   apiProxy.web(req, res, {target: `http://localhost:3006/api/description/${id}`});
 });
 
+app.get('api/photos/:id', function(req, res) {
+  let { id } = req.params;
+  console.log('photos endpoint hit with id: ', id);
+  apiProxy.web(req, res, { target: `http://localhosst:4984/api/photos/${id}` });
+});
 
 apiProxy.on('error', function(err) {
   console.log('proxy error: ', err);
